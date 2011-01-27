fb15b9feb6afaf3268fc10e4dfbc7d407ad965ed
周明智 <coldstars@msn.com>
2011-01-27 15:30:26 +0800

    完善$.ui.dynamictree

--- development/ui/jquery.ui.dynamictree.js
+++ development/ui/jquery.ui.dynamictree.js
@@ -1,16 +1,27 @@
 $.widget("ui.dynamictree", {
 	options: {
-		dynamic: true,
 		subTreeClassName: "ui-dtree-subtree",
 		folderClassName: "ui-dtree-folder",
-		loadingClassName: "ui-dtree-loading"
-	},
-
-	_init: function() {
+		loadingClassName: "ui-dtree-loading",
+		rootClassName: "ui-dtree-root",
+		rootData: "",
+		rootLabel: "主菜单",
+		expandRoot: false
 	},
 
 	_create: function() {
-		this_initSource();
+		this._initSource();
+		this._renderRoot();
+		this.element.append(this.root);
+	},
+
+	_init: function() {
+	    var o = this.options;
+		if(o.expandRoot) {
+		    this._expand(this.root.find('.' + o.folderClassName));
+        } else {
+		    this._initFolder(this.root);
+        }
 	},
 
 	_initSource: function() {
@@ -24,7 +35,7 @@ $.widget("ui.dynamictree", {
 				if(self.xhr) {
 					self.xhr.abort();
 				}
-				self.xhr = $.getJSON(source, request, function(data, status, xhr) {
+				self.xhr = $.getJSON(source, { id: request.id }, function(data, status, xhr) {
 					if(xhr === self.xhr) {
 						response(data);
 					}
@@ -34,39 +45,73 @@ $.widget("ui.dynamictree", {
 			return true;
 		}
 		if($.isArray(source)) {
-			o.dynamic = false;
-			return true;
+		    o.rootData = source;
+		    this.load = function(request, response) {
+		        response(request.child);
+		    }
+		}
+	},
+	
+	_normalize: function(data) {
+	    var child = data.child;
+	    data.hasChild = (child && $.isArray(child) && child.length > 0);
+		return data;
+	},
+	
+	_renderRoot: function() {
+		var o = this.options,
+			root = this.root = $('<ul class="' + o.rootClassName + '"></ul>'),
+			rootData = o.rootData;
+		if($.isArray(rootData)) {
+	        var len = rootData.length;
+	        for(var i = 0; i < len; i++) {
+		        this._renderItem(root, this._normalize(rootData[i]));
+	        }
+	        o.expandRoot = false;
+		} else {
+			this._renderItem(root, { id: o.rootData, name: o.rootLabel, hasChild: true });
 		}
 	},
 
-	_renderTree: function(parent, data) {
+	_renderSubTree: function(parent, data) {
 		var len = data.length, o = this.options, 
-			tree = $('<ul class="' + o.subTreeClassName + '"></ul>');
+			root = $('<ul class="' + o.subTreeClassName + '"></ul>');
 		for(var i = 0; i < len; i++) {
-			var item = data[i];
-				li = $('<li></li>').data("item.dynamictree", item);
-			if(item.hasChild == "true") {
-				li.append('<a href="#" class="' + o.folderClassName + '"></a>');
-			}
-			li.append('<a href="#">' + item.name + '</a>');
-			tree.append(li);
+			this._renderItem(root, this._normalize(data[i]));
 		}
-		parent.append(tree);
-		return tree;
+		parent.append(root);
+		return root;
+	},
+	
+	_renderItem: function(root, data) {
+		var li = $('<li></li>').data("item.dynamictree", data),
+		    o = this.options;
+		if(data.hasChild == true) {
+			li.append('<a href="#" class="' + o.folderClassName + '"></a>');
+		}
+		li.append('<a href="#">' + data.name + '</a>');
+		root.append(li);   
+		return li; 
+	},
+	
+	_expand: function(button) {
+	    var o = this.options,
+	        self = this,
+	        parent = button.addClass(o.loadingClassName).parent(),
+	        item = parent.data('item.dynamictree');
+	        
+		this.load(item, function(data) {
+			var tree = self._renderSubTree(parent, data);
+			self._initFolder(tree);
+			button.removeClass(o.loadingClassName);
+		});
 	},
 
-	_initFolder: function(tree) {
+	_initFolder: function(root) {
 		var o = this.options, self = this;
-		tree.find(o.folderClassName).one("click", function(e) {
+		root.find('.' + o.folderClassName).one("click", function(e) {
 			e.preventDefault();
-			var parent = $(this).addClass(o.loadingClassName),
-				item = parent.data('item.dynamictree');
-			
-			self.load({ id: item.id }, function(data) {
-				var tree = self._renderTree(parent, data);
-				_initFolder(tree);
-				parent.removeClass(o.loadingClassName);
-			});
+			self._expand($(this));
 		});
 	}
 });
